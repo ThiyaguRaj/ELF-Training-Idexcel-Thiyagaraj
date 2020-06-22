14e0766eac875c5efa72dd68018d55488edd30b8
Thiyagu <thiyagu5334raj@gmail.com>
2020-06-22 14:27:07 +0530

    edit

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,17 @@
-var num=10;
-console.log("value is = ",num);
-var str=`string`;
-console.log("the val is : "+str);
-var valll;
-console.log(valll);
+var a=1000,b=2000,c=3000;
+console.log(c);
+// if(a>c || b>c)
+// {
+//    if(a>b)
+//    {
+//        console.log('a is greater');
+//    }
+//    else{
+//      console.log('b is greater');
+//    }
+// }
+// else{
+//     console.log('c is greater');
+// }
+var res=(a>c || b>c)?((a>b)?"a is greater":"b is greater"):"c is greater";
+console.log(`result is : ${res} : so that is`);
