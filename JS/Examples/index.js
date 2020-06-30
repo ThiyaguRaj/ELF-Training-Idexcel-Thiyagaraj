8f3e48e36dec05f8fe874493b64ecd48a4655e77
Thiyagu <thiyagu5334raj@gmail.com>
2020-06-30 16:34:03 +0530

    notes

--- JS/Examples/index.js
+++ JS/Examples/index.js
@@ -15,12 +15,42 @@
 // }
 // var res=(a>c || b>c)?((a>b)?"a is greater":"b is greater"):"c is greater";
 // console.log(`result is : ${res} : so that is`);
-var person={
-    'name' : "abc",
-    'age' : 20
-}
-for(const key in person)
-{
-    console.log(person[key]);
+// var person={
+//     'name' : "abc",
+//     'age' : 20
+// }
+// for(const key in person)
+// {
+//     console.log(person[key]);
     
-}
+// }
+
+var arr=[0,1,2,3,4,5,6,7,80000000];
+console.log(arr);
+//SPLICE
+// arr.splice(3,3,`deno`);
+// console.log(arr);
+
+// SLICE
+// console.log(arr.slice(2,7));
+// console.log(arr);
+
+//FOR EACH
+
+// function add(a,b){
+//     return a+b;
+// }
+// function avg(func)
+// {
+//     return func/2;
+// }
+// console.log(avg(add(50,200)));
+// var newVal = arr.map(function(value){
+//     return value*10;
+// })
+// console.log(newVal);
+
+var str='    abcd     ';
+// var str1=str.trim();
+// console.log(str1);
+console.log(str.length);
